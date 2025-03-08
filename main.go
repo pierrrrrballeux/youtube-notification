1a1bdd0ed0b48167a3abdcc6395b463f239b1cd2
pierrrrrballeux <morie.sano215@gmail.com>
2025-03-09 06:44:38 +0900

    s

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"encoding/xml"
 	"log"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func handleCallback(c *gin.Context){
 	challenge, _ := c.GetQuery("hub.challenge")
-	log.Println("Challenge: %s", challenge)
+	log.Printf("Challenge: %s\n", challenge)
 	c.String(200, challenge)
 	return
 }
@@ -24,17 +26,25 @@ func handleNotification(c *gin.Context){
 		log.Println(err)
 	}
 
-	log.Println("%sが\n「%s」をアップロードしました!!!", result.Author.Name,result.Title)
+	log.Printf("%sが\n「%s」をアップロードしました!!!\n", result.Author.Name,result.Title)
 
 	c.String(200, "success")
 	return
 }
 
 func handleSubscribe(c *gin.Context){
+	_, err := http.Post("https://pubsubhubbub.appspot.com/", "application/x-www-form-urlencoded", nil)
+
+	if err != nil {
+		log.Println(err)
+		return 
+	}
+
 	return
 }
 
 func main(){
+	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 
 	r.GET("/", func(ctx *gin.Context) { ctx.String(200, "Hello World!!!") })
